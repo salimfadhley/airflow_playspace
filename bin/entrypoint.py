e40a7012d550f74f4fcdd747dd9ad1379a1475bc
sal <slpg500a>
2019-02-14 13:09:46 +0000

    Now can bootstrap the application

--- bin/entrypoint.py
+++ bin/entrypoint.py
@@ -1,25 +1,35 @@
 #! /usr/ocal/bin/python
 import os
 import subprocess
+import sys
 
-def main():
+import click
+
+AIRFLOW_COMMAND = "/usr/local/bin/airflow"
+
+@click.command()
+@click.argument("mode")
+def main(mode:str):
+    if mode=="webserver":
+        command = f"{AIRFLOW_COMMAND} webserver -p 8080"
+    elif mode=="scheduler":
+        command = f"{AIRFLOW_COMMAND} scheduler"
+    else:
+        raise RuntimeError(f"Invalid mode: {repr(mode)}")
+
     airflow_home_dir = os.environ.get("AIRFLOW_HOME")
     airflow_config_file = os.path.join(airflow_home_dir, "airflow.cfg")
 
-    if os.path.exists(airflow_config_file):
+    if not os.path.exists(airflow_config_file):
         print(f"Installing Airflow to {airflow_home_dir}.")
+        subprocess.call(f"{AIRFLOW_COMMAND} initdb", shell=True)
     else:
         print(os.listdir(airflow_home_dir))
         print("Airflow is already installed, skipping initialization.")
 
-    # airflow initdb
-    #
-    # aiflow webserver -p 8080
-    #
-    # ls -l ${airflow_home_dir}
-    #
-    # airflow scheduler
 
+
+    sys.exit(subprocess.call(command, shell=True))
 
 
 if __name__ == "__main__":
